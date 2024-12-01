0a5ed828b3e47b7192d4425f96e607e7dff9cd0c
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2024-12-01 19:59:55 +0700

    update code

--- Esp8266NodeMCUWebsocket/Esp8266NodeMCUWebsocket.ino
+++ Esp8266NodeMCUWebsocket/Esp8266NodeMCUWebsocket.ino
@@ -11,142 +11,186 @@
 #include <ESPAsyncWebServer.h>
 
 // Replace with your network credentials
-const char* ssid     = "LAM HOUSE";
-const char* password = "xinchaoban";
+const char* ssid     = "HB.403";
+const char* password = "Dhsg@HB403";
 
 bool ledState = 0;
+bool ledState2 = 0;
 const int ledPin = 5;
+const int ledPin2 = 4;
+int D0_Value = D8;
+int raw_Value = A0;
+int Set_point = 120;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
+
 <head>
-  <title>ESP Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-  html {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-  }
-  h1 {
-    font-size: 1.8rem;
-    color: white;
-  }
-  h2{
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: #143642;
-  }
-  .topnav {
-    overflow: hidden;
-    background-color: #143642;
-  }
-  body {
-    margin: 0;
-  }
-  .content {
-    padding: 30px;
-    max-width: 600px;
-    margin: 0 auto;
-  }
-  .card {
-    background-color: #F8F7F9;;
-    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
-    padding-top:10px;
-    padding-bottom:20px;
-  }
-  .button {
-    padding: 15px 50px;
-    font-size: 24px;
-    text-align: center;
-    outline: none;
-    color: #fff;
-    background-color: #0f8b8d;
-    border: none;
-    border-radius: 5px;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-   }
-   /*.button:hover {background-color: #0f8b8d}*/
-   .button:active {
-     background-color: #0f8b8d;
-     box-shadow: 2 2px #CDCDCD;
-     transform: translateY(2px);
-   }
-   .state {
-     font-size: 1.5rem;
-     color:#8c8c8c;
-     font-weight: bold;
-   }
-  </style>
-<title>ESP Web Server</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="data:,">
+    <title>ESP Web Server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        html {
+            font-family: Arial, Helvetica, sans-serif;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 1.8rem;
+            color: white;
+        }
+
+        h2 {
+            font-size: 1.5rem;
+            font-weight: bold;
+            color: #143642;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #143642;
+        }
+
+        body {
+            margin: 0;
+        }
+
+        .content {
+            padding: 30px;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        .card {
+            background-color: #F8F7F9;
+            ;
+            box-shadow: 2px 2px 12px 1px rgba(140, 140, 140, .5);
+            padding-top: 10px;
+            padding-bottom: 20px;
+        }
+
+        .button {
+            padding: 15px 50px;
+            font-size: 24px;
+            text-align: center;
+            outline: none;
+            color: #fff;
+            background-color: #0f8b8d;
+            border: none;
+            border-radius: 5px;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        }
+
+        /*.button:hover {background-color: #0f8b8d}*/
+        .button:active {
+            background-color: #0f8b8d;
+            box-shadow: 2 2px #CDCDCD;
+            transform: translateY(2px);
+        }
+
+        .state {
+            font-size: 1.5rem;
+            color: #8c8c8c;
+            font-weight: bold;
+        }
+    </style>
+    <title>ESP Web Server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
 </head>
+
 <body>
-  <div class="topnav">
-    <h1>ESP WebSocket Server</h1>
-  </div>
-  <div class="content">
-    <div class="card">
-      <h2>Output - GPIO 5</h2>
-      <p class="state">state: <span id="state">%STATE%</span></p>
-      <p><button id="button" class="button">Toggle</button></p>
+    <div class="topnav">
+        <h1>ESP WebSocket Server</h1>
     </div>
-  </div>
-<script>
-  var gateway = `ws://${window.location.hostname}/ws`;
-  var websocket;
-  window.addEventListener('load', onLoad);
-  function initWebSocket() {
-    console.log('Trying to open a WebSocket connection...');
-    websocket = new WebSocket(gateway);
-    websocket.onopen    = onOpen;
-    websocket.onclose   = onClose;
-    websocket.onmessage = onMessage; // <-- add this line
-  }
-  function onOpen(event) {
-    console.log('Connection opened');
-  }
-  function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
-  }
-  function onMessage(event) {
-    var state;
-    if (event.data == "1"){
-      state = "ON";
-    }
-    else{
-      state = "OFF";
-    }
-    document.getElementById('state').innerHTML = state;
-  }
-  function onLoad(event) {
-    initWebSocket();
-    initButton();
-  }
-  function initButton() {
-    document.getElementById('button').addEventListener('click', toggle);
-  }
-  function toggle(){
-    websocket.send('toggle');
-  }
-</script>
+    <div class="content">
+        <div class="card">
+            <h2>LED 1</h2>
+            <p class="state">state: <span id="state1">%STATE1%</span></p>
+            <p><button id="button1" class="button">Toggle</button></p>
+        </div>
+        <div class="card">
+            <h2>LED 2</h2>
+            <p class="state">state: <span id="state">%STATE%</span></p>
+            <p><button id="button" class="button">Toggle</button></p>
+        </div>
+    </div>
+    <script>
+        var gateway = `ws://${window.location.hostname}/ws`;
+        var websocket;
+        window.addEventListener('load', onLoad);
+        function initWebSocket() {
+            console.log('Trying to open a WebSocket connection...');
+            console.log(gateway);
+            websocket = new WebSocket(gateway);
+            websocket.onopen = onOpen;
+            websocket.onclose = onClose;
+            websocket.onmessage = onMessage; // <-- add this line
+        }
+        function onOpen(event) {
+            console.log('Connection opened');
+        }
+        function onClose(event) {
+            console.log('Connection closed');
+            setTimeout(initWebSocket, 2000);
+        }
+        function onMessage(event) {
+            var state;
+            var state1;
+            if (event.data == "LED0 1") {
+                state = "ON";
+            }else if (event.data == "LED0 0") {
+                state = "OFF";
+            }
+                
+            if (event.data == "LED1 0") {
+                state1 = "OFF";
+            } else if (event.data == "LED1 1") {
+                state1 = "ON";
+            }
+
+            
+            if (state1!=undefined){
+                document.getElementById('state1').innerHTML = state1;
+            }
+            if (state!=undefined){
+                document.getElementById('state').innerHTML = state;
+            }
+            
+        }
+        function onLoad(event) {
+            initWebSocket();
+            initButton();
+        }
+        function initButton() {
+            document.getElementById('button').addEventListener('click', toggle);
+            document.getElementById('button1').addEventListener('click', toggle1);
+        }
+        function toggle() {
+            websocket.send('toggle');
+        }
+        function toggle1() {
+            websocket.send('toggle1');
+        }
+    </script>
 </body>
+
 </html>
 )rawliteral";
 
 void notifyClients() {
-  ws.textAll(String(ledState));
+  ws.textAll("LED0 "+String(ledState));
+  ws.textAll("LED1 " +String(ledState2));
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
@@ -155,6 +199,10 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     data[len] = 0;
     if (strcmp((char*)data, "toggle") == 0) {
       ledState = !ledState;
+      notifyClients();
+    }
+    if (strcmp((char*)data, "toggle1") == 0) {
+      ledState2 = !ledState2;
       notifyClients();
     }
   }
@@ -195,13 +243,32 @@ String processor(const String& var){
   }
   return String();
 }
+String processor2(const String& var){
+  Serial.println(var);
+  if(var == "STATE1"){
+    if (ledState2){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
+}
 
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
 
+  // Sensors 
+  pinMode(D0_Value, OUTPUT); // Set pin D8 as digital output
+  pinMode(raw_Value, INPUT);
+
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
+
+  pinMode(ledPin2, OUTPUT);
+  digitalWrite(ledPin2, LOW);
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -218,6 +285,7 @@ void setup(){
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
+    request->send_P(200, "text/html", index_html, processor2);
   });
 
   // Start server
@@ -227,4 +295,10 @@ void setup(){
 void loop() {
   ws.cleanupClients();
   digitalWrite(ledPin, ledState);
+   digitalWrite(ledPin2, ledState2);
+     int val_digital = digitalRead(D0_Value);
+  int val_analog = analogRead(raw_Value );
+    Serial.print(val_analog);
+  Serial.print("\t");
+  Serial.println(val_digital);
 }
